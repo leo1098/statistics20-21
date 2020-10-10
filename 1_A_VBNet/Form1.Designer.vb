db38a17b4711f5a8a5d4b0742e062244358f5089
leo1098 <leo1098@yahoo.it>
2020-10-10 23:00:09 +0200

    Added Drag&Drop functionality

--- 1_A_VBNet/Form1.Designer.vb
+++ 1_A_VBNet/Form1.Designer.vb
@@ -22,45 +22,77 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(70, 44)
+        Me.Button1.Location = New System.Drawing.Point(62, 35)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(107, 43)
+        Me.Button1.Size = New System.Drawing.Size(95, 34)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Put Text"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(70, 164)
+        Me.Button2.Location = New System.Drawing.Point(62, 104)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(107, 47)
+        Me.Button2.Size = New System.Drawing.Size(95, 38)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Clear Text"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(301, 44)
+        Me.RichTextBox1.Location = New System.Drawing.Point(268, 26)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(386, 176)
+        Me.RichTextBox1.Size = New System.Drawing.Size(386, 116)
         Me.RichTextBox1.TabIndex = 2
         Me.RichTextBox1.Text = ""
         '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.SystemColors.Menu
+        Me.RichTextBox2.Location = New System.Drawing.Point(268, 185)
+        Me.RichTextBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
+        Me.RichTextBox2.Size = New System.Drawing.Size(386, 88)
+        Me.RichTextBox2.TabIndex = 4
+        Me.RichTextBox2.Text = resources.GetString("RichTextBox2.Text")
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Drag&Drop a file here to show its path"})
+        Me.ListBox1.Location = New System.Drawing.Point(268, 300)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(386, 84)
+        Me.ListBox1.TabIndex = 5
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(711, 406)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.RichTextBox2)
         Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -70,4 +102,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents RichTextBox1 As RichTextBox
+    Private WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- 1_A_VBNet/Form1.vb
+++ 1_A_VBNet/Form1.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.RichTextBox1.Text = "A Sample Text :)"
     End Sub
@@ -15,4 +19,27 @@
     Private Sub RichTextBox1_MouseLeave(sender As Object, e As EventArgs) Handles RichTextBox1.MouseLeave
         Me.RichTextBox1.BackColor = Color.White
     End Sub
+
+    Private Sub ListBox1_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox1.DragEnter
+        'If it contains a String type
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            'Copy
+            e.Effect = DragDropEffects.Copy
+        Else
+            'Otherwise undo the action
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox1.DragDrop
+        'Get strings in memory
+        Dim S() As String = e.Data.GetData(DataFormats.FileDrop)
+        'Put the file's path in the list box
+        For Each path In S
+            Me.ListBox1.Items.Add(path)
+        Next
+
+    End Sub
+
+
 End Class
